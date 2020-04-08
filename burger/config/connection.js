8aec1a0dd0aa74855f34172591a934e6dc455391
Kiyole <Kasmith8100@gmail.com>
2020-04-08 17:36:57 -0400

    fixing connection file

--- burger/config/connection.js
+++ burger/config/connection.js
@@ -1,12 +1,21 @@
 var mysql = require("mysql");
 
- var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+   var connection = mysql.createConnection({
    host: "localhost",
    port: 3306,
    user: "root",
    password: "root",
    database: "burgersDB"
- });
+  });
+};
+
+
+
+
 
 
 connection.connect(function(err) {
